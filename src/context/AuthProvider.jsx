0e982933b482bcc243234a0429b8996ed661a1cd
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-01-06 17:25:24 +0100

    good

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,32 +1,28 @@
 // AuthProvider.jsx
 
-import React, { createContext, useContext, useState } from 'react';
-
-
+import React, { createContext, useContext, useState } from "react";
 
 export const getUserLocalStorage = () => {
   return JSON.parse(localStorage.getItem("user")) || null;
 };
 
- 
-
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    const [admin, setAdmin] = useState(getUserLocalStorage());
-    const [auth, setAuth] = useState({
-        // Default or initial authentication state
-        phoneNoOrEmail: '',
-        password: '',
-        roles: [],
-        accessToken: '',
-    });
-//   console.log(admin)
-    return (
-        <AuthContext.Provider value={{ auth, setAuth, admin }}>
-            {children}
-        </AuthContext.Provider>
-    ); 
+  const [admin, setAdmin] = useState(getUserLocalStorage());
+  const [auth, setAuth] = useState({
+    // Default or initial authentication state
+    phoneNoOrEmail: "",
+    password: "",
+    roles: [],
+    accessToken: "",
+  });
+  //   console.log(admin)
+  return (
+    <AuthContext.Provider value={{ auth, setAuth, admin }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 export default AuthContext;
 
