5cf3e1688deb413679d7895f51e6401dc4dfe172
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-02-23 23:09:58 +0100

    order update

--- src/components/loginPage/LoginPage.jsx
+++ src/components/loginPage/LoginPage.jsx
@@ -5,10 +5,12 @@ import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./loginPage.css";
-import axios from '../../api/axiosInstance';
+import axios from "../../api/axiosInstance";
+import { useAuthContext } from "../../context/AuthProvider";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const { setAdmin, admin } = useAuthContext();
   const [showPassword, setShowPassword] = useState(false);
   const [credential, setCredential] = useState({
     phoneNoOrEmail: "",
@@ -36,7 +38,7 @@ const LoginPage = () => {
 
       localStorage.setItem("user", JSON.stringify(data));
       setCredential(data);
-
+      setAdmin(data);
       toast.success("Login successful!");
 
       setTimeout(() => {
@@ -79,14 +81,14 @@ const LoginPage = () => {
         </div>
         <div className="second-inp">
           <input
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             onChange={handleChange}
             name="password"
             value={credential.password}
             placeholder="password"
           />
           <span className="eye-icon">
-            <RemoveRedEyeIcon onClick={handleTogglePassword}/>
+            <RemoveRedEyeIcon onClick={handleTogglePassword} />
           </span>
         </div>
         <div className="remember">
